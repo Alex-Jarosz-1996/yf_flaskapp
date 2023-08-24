9d3bf5fdf2f1487785e7fccd56057d2d00a99c78
alexj <alexj@LAPTOP-7048P6HN>
2023-08-24 16:56:28 +1000

    changed reference to csv files

--- src/common/get_tickers.py
+++ src/common/get_tickers.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 def get_all_asx_tickers():
     current_dir = os.path.dirname(__file__)
-    csv_file_path = os.path.join(current_dir, '../company_lists/asx_companies.csv')
+    csv_file_path = os.path.join(current_dir, '../../company_lists/asx_companies.csv')
     df = pd.read_csv(csv_file_path)
     return df["ASX code"]
 
 def get_all_us_tickers():
     current_dir = os.path.dirname(__file__)
-    csv_file_path = os.path.join(current_dir, '../company_lists/us_companies.csv')
+    csv_file_path = os.path.join(current_dir, '../../company_lists/us_companies.csv')
     df = pd.read_csv(csv_file_path)
     return df["Symbol"]
