64a934fbb4ffe489aa0f7c381854a563e1dd680f
alexj <alexj@LAPTOP-7048P6HN>
2023-08-24 17:16:36 +1000

    added cleanse stock list method

--- src/common/get_tickers.py
+++ src/common/get_tickers.py
@@ -12,3 +12,17 @@ def get_all_us_tickers():
     csv_file_path = os.path.join(current_dir, '../../company_lists/us_companies.csv')
     df = pd.read_csv(csv_file_path)
     return df["Symbol"]
+
+def cleanse_stocklist(stocks: pd.DataFrame):
+    for stock in stocks:
+        if not isinstance(stock, str):
+            stock = str(stock)
+        
+        if " " in stock:
+            stock = stock.replace(" ", "")
+        elif "\n" in stock:
+            stock = stock.replace("\n", "")
+        else:
+            pass  # potentially need to do regex checks here
+
+    return stocks
