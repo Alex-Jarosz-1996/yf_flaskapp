bd0b1e21efb9f8e9c162e817e6506001c26c32b2
alexj <alexj@LAPTOP-7048P6HN>
2023-08-29 08:11:11 +1000

    added init file in src/common folder. Added additional methods in get tickers module

--- src/common/get_tickers.py
+++ src/common/get_tickers.py
@@ -2,6 +2,8 @@ import json
 import os
 import pandas as pd
 
+from typing import List
+
 
 def get_all_asx_tickers():
     current_dir = os.path.dirname(__file__)
@@ -35,3 +37,13 @@ def cleanse_stocklist(stocks: pd.DataFrame) -> pd.DataFrame:
 def jsonify_stocklist(stocks: pd.DataFrame) -> pd.DataFrame:
     stocks = list(stocks)
     return json.dumps(stocks, indent=1)
+
+
+def cleansed_asx_stocklist() -> List[str]:
+    codes = get_all_asx_tickers()
+    return list(cleanse_stocklist(codes))
+
+
+def cleansed_us_stocklist() -> List[str]:
+    codes = get_all_us_tickers()
+    return list(cleanse_stocklist(codes))
